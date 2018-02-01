66bb347f251042ad1e10a78bbbbfe9feec7b6780
thedeveloper-sharath <sharahprakash3@gmail.com>
2018-02-01 23:34:11 +0530

    Gradle Plugin & Build Tool Update

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,13 @@
 package com.londonappbrewery.magiceightball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView imageView = findViewById(R.id.ball_image);
+        imageView.setVisibility(View.INVISIBLE);
+        final int[] imagearray = {R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5};
+        final Button ask_button = findViewById(R.id.ask_button);
+        final TextView textView = findViewById(R.id.ask_text);
+        ask_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setVisibility(View.GONE);
+                Random randomnumbergenerator = new Random();
+
+                int number = randomnumbergenerator.nextInt(5);
+                imageView.setImageResource(imagearray[number]);
+                imageView.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
